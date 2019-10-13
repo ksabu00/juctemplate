a30e6c2558ea860ff530482ffdf811c7c7898cf8
ksabu00 <18229712137@189.cn>
2019-10-13 17:39:11 +0800

    Initial commit

---
+++ JavaWeb02/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 18229
+  Date: 2019/10/13
+  Time: 16:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>ksabu大数据物联网平台</title>
+  </head>
+  <body>
+  <h1 style="color: red">感谢你的访问，网站维护中呦！</h1>
+  </body>
+</html>
---
+++ juctemplate/src/main/java/com/ksabu/project1/HelloWorld.java
@@ -0,0 +1,56 @@
+package com.ksabu.project1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class HelloWorld {
+
+    public static final int NUM = 18;
+    /**
+     *
+     *
+     */
+
+     private int num = 12;
+        
+
+    /**
+     *
+     */
+     private String str1;
+     
+    // 第一个hello程序
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        ArrayList list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        if (list == null) {
+
+            
+        }
+
+        for (Object o : list) {
+            System.out.println("o = " + o);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            
+        }
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        
+        HelloWorld.getName();
+
+    }
+
+    // 获取名字的方法
+    public static void getName(){
+        int[] str = new int[]{1, 2, 3, 4};
+        for (int i = 0; i < str.length; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
